b6b1f8cc513bec71772be1043bd2b527075df5e0
yuhijackman <yuuhiandlisa@gmail.com>
2025-05-25 19:55:58 +0900

    🔧 Set up drizzle kit

---
+++ db/index.ts
@@ -0,0 +1,10 @@
+import { schema } from "@/db/schema";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+	throw new Error("DATABASE_URL environment variable is not set");
+}
+export const client = postgres(connectionString, { prepare: false });
+export const db = drizzle(client, { schema });
