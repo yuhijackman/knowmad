eb694a51842afea2634a543f6d5a0005cb2445b9
yuhijackman <yuuhiandlisa@gmail.com>
2025-06-20 16:51:06 +0900

    🪨define constant variables

---
+++ constants/routes.ts
@@ -0,0 +1,5 @@
+export const LOGIN_PATH = "/login";
+export const SIGN_UP_PATH = "/sign-up";
+export const DASHBOARD_PATH = "/dashboard";
+export const ONBOARDING_PATH = "/onboarding";
+export const SIGN_UP_SUCCESS_PATH = "/sign-up-success";
