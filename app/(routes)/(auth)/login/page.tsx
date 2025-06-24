a7be2a8cf4d30f459f69a378e9f23b2b92c605fb
yuhijackman <yuuhiandlisa@gmail.com>
2025-06-24 16:20:27 +0900

    Disable google and github sign in

--- app/(routes)/(auth)/login/page.tsx
+++ app/(routes)/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { Divider, Group, Paper, Text } from "@mantine/core";
+import { Alert, Divider, Group, Paper, Stack, Text } from "@mantine/core";
 import { GithubButton } from "@/features/authentication/github-button";
 import { GoogleButton } from "@/features/authentication/google-button";
 import { LoginForm } from "@/features/authentication/login-form";
@@ -6,14 +6,22 @@ import { LoginForm } from "@/features/authentication/login-form";
 export default function LoginPage() {
 	return (
 		<Paper radius="md" p="lg" withBorder={true} maw={400} mx="auto" my="xl">
-			<Text size="lg" fw={500} ta="center" mb="md">
-				Welcome back!
-			</Text>
-
-			<Group grow={true} mb="md" mt="md">
-				<GoogleButton radius="xl">Google</GoogleButton>
-				<GithubButton radius="xl">GitHub</GithubButton>
-			</Group>
+			<Stack>
+				<Alert variant="light" color="orange" title="Warning">
+					Sign-in with Google and GitHub is coming soon!
+				</Alert>
+				<Text size="lg" fw={500} ta="center" mb="md">
+					Welcome back!
+				</Text>
+				<Group grow={true} mb="md" mt="md">
+					<GoogleButton disabled={true} radius="xl">
+						Google
+					</GoogleButton>
+					<GithubButton disabled={true} radius="xl">
+						GitHub
+					</GithubButton>
+				</Group>
+			</Stack>
 
 			<Divider label="Or continue with email" labelPosition="center" my="lg" />
 
--- app/(routes)/(auth)/sign-up/page.tsx
+++ app/(routes)/(auth)/sign-up/page.tsx
@@ -1,4 +1,4 @@
-import { Divider, Group, Paper, Text } from "@mantine/core";
+import { Alert, Divider, Group, Paper, Stack, Text } from "@mantine/core";
 import { GithubButton } from "@/features/authentication/github-button";
 import { GoogleButton } from "@/features/authentication/google-button";
 import { SignUpForm } from "@/features/authentication/sign-up-form";
@@ -6,14 +6,22 @@ import { SignUpForm } from "@/features/authentication/sign-up-form";
 export default function SignUpPage() {
 	return (
 		<Paper radius="md" p="lg" withBorder={true} maw={400} mx="auto" my="xl">
-			<Text size="lg" fw={500} ta="center" mb="md">
-				Register with
-			</Text>
-
-			<Group grow={true} mb="md" mt="md">
-				<GoogleButton radius="xl">Google</GoogleButton>
-				<GithubButton radius="xl">GitHub</GithubButton>
-			</Group>
+			<Stack>
+				<Alert variant="light" color="orange" title="Warning">
+					Sign-up with Google and GitHub is coming soon!
+				</Alert>
+				<Text size="lg" fw={500} ta="center" mb="md">
+					Register with
+				</Text>
+				<Group grow={true} mb="md" mt="md">
+					<GoogleButton disabled={true} radius="xl">
+						Google
+					</GoogleButton>
+					<GithubButton disabled={true} radius="xl">
+						GitHub
+					</GithubButton>
+				</Group>
+			</Stack>
 
 			<Divider label="Or continue with email" labelPosition="center" my="lg" />
 
